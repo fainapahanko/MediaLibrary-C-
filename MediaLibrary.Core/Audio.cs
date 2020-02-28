76ea9728ec890ff19127938b4c1ab6efa69776af
fainapahanko <f.poganko@gmail.com>
2020-02-28 16:20:40 +0100

    first commit

---
+++ MediaLibrary.Core/Audio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace MediaLibrary.Core
+{
+    public class Audio : Item
+    {
+        public string Duration;
+
+        public Audio(string Name, string Author, int ReleaseDate, bool Loan, string Duration) 
+        {
+            this.Name = Name;
+            this.Author = Author;
+            this.ReleaseDate = ReleaseDate;
+            this.Loan = Loan;
+            this.Duration = Duration;
+        }
+    }
+}
---
+++ MediaLibrary.Core/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace MediaLibrary.Core
+{
+    public class Book : Item
+    {
+        public int NumberOfPages;
+
+        public Book(string Name, string Author, int ReleaseDate, bool Loan, int NumberOfPages)
+        {
+            this.Name = Name;
+            this.Author = Author;
+            this.ReleaseDate = ReleaseDate;
+            this.Loan = Loan;
+            this.NumberOfPages = NumberOfPages;
+        }
+    }
+}
